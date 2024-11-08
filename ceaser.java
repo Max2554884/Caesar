e41ba041e8cbec63f48f3b26d455a429bbf7a691
max2554884 <max.wurmbach@t-online.de>
2024-11-08 10:40:58 +0100

    p

---
+++ ceaser.java
@@ -0,0 +1,28 @@
+public class ceaser {
+
+    private String kt;
+    private int s;
+    private String gt;
+    public void ceasar(){};
+    public void verschluesseln (){
+
+    };
+    public void entschluesseln (){}
+    public int getASCII (char pWert)
+    {return};
+
+    public void setKT (String pWert){}
+    public String getKt ()
+    {
+        return
+    };
+    public void setS (int pWert){}
+    private char getChar(int pWert)
+    {
+        return (char)pWert;
+    }
+    private int getS(char pWert)
+    {
+        return (int)pWert;
+    }
+}
