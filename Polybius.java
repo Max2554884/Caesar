cf5f542dd395a0e93c0e802095824b544f30bcbb
max2554884 <max.wurmbach@t-online.de>
2024-12-03 12:42:24 +0100

    polybius hinzu ; fertig

--- Polybius.java
+++ Polybius.java
@@ -1,2 +1,34 @@
-public class Polybius {
+public class Polybius extends Kryptomat {
+    @Override
+    public void verschluesseln() {
+
+    }
+
+    @Override
+    public void entschluesseln() {
+
+    }
+
+ int[][] alphabetQuadrat;
+Polybius() {
+
+
+}
+
+
+
+
+public void bfms() {
+
+
+    char a = 97;
+    alphabetQuadrat= new int[5][5];
+    for (int zeile = 0; zeile < alphabetQuadrat.length-1 ; zeile++) {
+        for (int spalte = 0; spalte < alphabetQuadrat.length-1; spalte++) {
+
+        }
+    }
+
+
+}
 }
