195e2ea9d6e1bc86f0ef705d67c953a325dae454
max2554884 <max.wurmbach@t-online.de>
2024-12-04 11:21:15 +0100

    ja

--- Polybius.java
+++ Polybius.java
@@ -1,5 +1,6 @@
 public class Polybius extends Kryptomat {
     int[][] alphabetQuadrat;
+
     public  Polybius() {
         super();
         bfms();
@@ -7,11 +8,11 @@ public class Polybius extends Kryptomat {
     @Override
     //verschluesselung !
     public void verschluesseln() {
-
+       gt="";
         for (int i = 1; i <kt.length() ; i++) {//ghet durch Klartext durch
             for (int j = 1; j < alphabetQuadrat.length ; j++) {// zeile
                 for (int h = 1; h < alphabetQuadrat.length; h++) {//spalte
-                    if (kt.charAt(i) ==alphabetQuadrat[i][j]) {
+                    if (kt.charAt(i) ==alphabetQuadrat[j][h]) {
                         gt=gt+j+h;
 
                 }
@@ -37,9 +38,9 @@ public class Polybius extends Kryptomat {
 //befüllt beide dimensionene des arrays mit Buchstaben und legt den char wert fest
     public void bfms() {
         char a = 97;
-        alphabetQuadrat= new int[5][5];
-        for (int zeile = 0; zeile < alphabetQuadrat.length-1 ; zeile++) {
-            for (int spalte = 0; spalte < alphabetQuadrat.length-1; spalte++) {
+        alphabetQuadrat= new int[6][6];
+        for (int zeile = 1; zeile < alphabetQuadrat.length-1 ; zeile++) {
+            for (int spalte = 1; spalte < alphabetQuadrat.length-1; spalte++) {
                 if( a== 105){
                     a++;
                 }
@@ -49,8 +50,6 @@ public class Polybius extends Kryptomat {
 
     }
 
-    @Override
-    public String getGt() {
-        return super.getGt();
-    }
+
+
 }
